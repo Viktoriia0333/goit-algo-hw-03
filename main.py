5d8bbe5997385d9590c918110e61a95e17d1ac1d
Viktoriia <v9bondarenko@gmail.com>
2024-11-20 23:03:52 +0200

    improve first and second tasks

--- main.py
+++ main.py
@@ -7,32 +7,30 @@ import re
 def get_days_from_today(date):
     current_date = datetime.now()
     try:
-        converted_date = datetime.strptime(date, '%Y.%m.%d')
+        converted_date = datetime.strptime(date, '%Y-%m-%d')
         days_from_today = current_date - converted_date
         return days_from_today.days
     except ValueError:
         return 'Please, enter a date in the right format'
 
 
-date = input('Enter date (format: \'РРРР.ММ.ДД\'): ')
+date = input('Enter date (format: \'РРРР-ММ-ДД\'): ')
 print(get_days_from_today(date))
 
 
 # second task
 def get_numbers_ticket(min, max, quantity):
     my_set = set()
-    if min < 1 or max > 1000:
-        return []
-    elif quantity > (max-min+1):
-        return f'There are no no {quantity} numbers in this range.'
-    else:
+    if min >= 1 and max <= 1000:
         while len(my_set) < quantity:
             i = random.randint(min, max)
             my_set.add(i)
+    elif quantity > (max-min+1):
+        return f'There are no {quantity} numbers in this range.'
     return list(my_set)
 
 
-print(get_numbers_ticket(1, 12, 20))
+print(get_numbers_ticket(1, 12, 5))
 
 
 # third task
